cd60ac5aae4748cb97851f7fa3a065f7f0bd75fe
hendisantika <hendisantika@yahoo.co.id>
2024-08-11 11:03:30 +0700

    Add updateContact 🫸🌀✏️📗🐧🐳⬆

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -17,4 +17,47 @@ export default function ContactForm({contact}: Props) {
         revalidatePath("/");
     }
 
+    async function updateContact(formData: FormData) {
+        "use server";
+        await prisma.contact.update({
+            where: {id: contact?.id},
+            data: {
+                name: formData.get("name") as string,
+                phone: formData.get("phone") as string,
+                city: formData.get("city") as string
+            }
+        });
+        revalidatePath(`/${contact?.id}/view`);
+    }
+
+    return (
+        <form
+            className="flex flex-col gap-2"
+            action={contact ? updateContact : addContact}>
+            <input
+                type="text"
+                name="name"
+                placeholder="Contact Name"
+                className="p-2 border border-black"
+                defaultValue={contact?.name}
+            />
+            <input
+                type="text"
+                name="phone"
+                placeholder="Contact Phone Number"
+                className="p-2 border border-black"
+                defaultValue={contact?.phone}
+            />
+            <input
+                type="text"
+                name="city"
+                placeholder="Enter City"
+                className="p-2 border border-black"
+                defaultValue={contact?.city}
+            />
+            <button type="submit" className="bg-blue-500 text-white rounded-md p-1">
+                {contact ? "Update Contact" : "Add Contact"}
+            </button>
+        </form>
+    );
 }
